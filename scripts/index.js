fc5d6d8b205b98de03fa9aa20e24021601668abe
TJahongir <jakhongirt@gmail.com>
2024-01-18 04:38:35 -0600

    added basic func and revised my skills

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,20 @@
-let name = document.getElementById("name");
-name.addEventListener("click", () => console.log("hello"));
+let gridContainer = document.getElementById("grid-container");
+for (i = 0; i < 798; i++) {
+	let box = document.createElement("div");
+	box.classList.add("box");
+	box.setAttribute("id", "box");
+
+	gridContainer.appendChild(box);
+}
+let boxes = document.querySelectorAll(".box");
+
+boxes.forEach(function (box) {
+	box.addEventListener("mouseenter", () => {
+		box.style.backgroundColor = "lightcoral";
+	});
+
+	// removes the color
+	// box.addEventListener("mouseleave", () => {
+	// 	box.style.backgroundColor = "cyan";
+	// });
+});
